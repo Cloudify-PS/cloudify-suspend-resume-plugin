d5b3f1eace73ca7d078869ab9235b7efe2fd6b1d
Isaac Shabtay <isaac@shabtay.com>
2017-12-28 16:58:22 -0800

    Review items

--- suspend_resume_plugin/tasks.py
+++ suspend_resume_plugin/tasks.py
@@ -1,6 +1,3 @@
-import time
-
-from cloudify import ctx
 from cloudify.exceptions import NonRecoverableError
 
 
@@ -11,30 +8,32 @@ class Values(object):
 WAITING = 'WAITING'
 
 
-def suspend(runtime_property,
+def suspend(ctx,
+            runtime_property,
             retry_interval,
             **kwargs):
     """
     Active polling on the runtime property
     :parameter: retry_interval in seconds
     """
-    ctx.instance.runtime_properties[WAITING] = True
-    ctx.logger.info('Runtime properties: {}'
-                    .format(str(ctx.instance.runtime_properties)))
-    while WAITING and ctx.instance.runtime_properties.get(runtime_property):
-        value = ctx.instance.runtime_properties.get(runtime_property)
+
+    runtime_props = ctx.instance.runtime_properties
+
+    runtime_props[WAITING] = True
+    ctx.logger.debug('Runtime properties: {}'
+                    .format(str(runtime_props)))
+
+    value = runtime_props.get(runtime_property)
+    if value:
+        runtime_props[WAITING] = False
         if value == Values.OK:
             ctx.logger.info("Polling received OK status. Operation succeeded.")
-            ctx.instance.runtime_properties[WAITING] = False
             return
-        elif value == Values.FAILED:
-            ctx.instance.runtime_properties[WAITING] = False
+        if value == Values.FAILED:
             raise NonRecoverableError("Polling received FAILED status. "
                                       "Operation failed.")
-        else:
-            ctx.instance.runtime_properties[WAITING] = False
-            raise NonRecoverableError("Unknown status '{}'. Operation failed."
-                                      .format(value))
+        raise NonRecoverableError("Unknown status '{}'. Operation failed."
+                                  .format(value))
     else:
         msg = "No status received. " \
               "Retrying polling after {} seconds.".format(retry_interval)
@@ -42,22 +41,25 @@ def suspend(runtime_property,
                             retry_after=retry_interval)
 
 
-def set_value(runtime_property,
-              set_value,
+def set_value(ctx,
+              runtime_property,
+              value,
               **kwargs):
-    ctx.instance.runtime_properties[runtime_property] = set_value
-    ctx.logger.info('Runtime properties: {}'
+    ctx.instance.runtime_properties[runtime_property] = value
+    ctx.logger.debug('Runtime properties: {}'
                     .format(str(ctx.instance.runtime_properties)))
 
 
-def fail(runtime_property,
+def fail(ctx,
+         runtime_property,
          **kwargs):
-    set_value(runtime_property, Values.FAILED, **kwargs)
+    set_value(ctx, runtime_property, Values.FAILED, **kwargs)
 
 
-def resume(runtime_property,
+def resume(ctx,
+           runtime_property,
            **kwargs):
-    set_value(runtime_property, Values.OK, **kwargs)
+    set_value(ctx, runtime_property, Values.OK, **kwargs)
 
 
 
